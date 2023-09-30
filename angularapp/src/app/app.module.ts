606954333523c916b17f473b558682084e329996
Iamneo <techadmin@iamneo.ai>
2023-09-30 04:25:28 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { AppRoutingModule } from './app-routing.module';
@@ -8,7 +9,7 @@ import { QuoteService } from './services/quote.service';
 
 @NgModule({
   declarations: [AppComponent, QuoteComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule], // Add FormsModule
+  imports: [BrowserModule, AppRoutingModule, FormsModule, CommonModule], // Add FormsModule
   providers: [QuoteService],
   bootstrap: [AppComponent],
 })
